5c7e2453d8de30a50171e393eb0e6793d76c3de5
sabeerkhan2003 <sabeersabee20@gmail.com>
2025-02-05 16:59:03 +0530

    carousel

--- src/Components/Home/Bg_Carousel.jsx
+++ src/Components/Home/Bg_Carousel.jsx
@@ -31,9 +31,9 @@ export function Bg_Carousel() {
     <>
       <div className="h-[100%] w-full bg-gradient-to-b from-blue-400 to-blue-500 opacity-90 mix-blend-multiply relative font- ">
         
-      <p className="font-bold lg:text-5xl w-fit   mx-auto absolute xl:top-[35%] text-white z-30 brightness-200 font-opensans text-3xl top-10 left-[30%] md:left-[30%] md:top-[27%] lg:top-[38%] lg:left-[25%] xl:left-[30%]">Admissions  <br className="md:hidden" /><span className="font-Roboto relative left-3 md:left-0 font-bold w-fit  text-[#A02021]">Open 2025</span></p>
+      <p className="font-bold lg:text-5xl w-fit   mx-auto absolute xl:top-[35%] text-white z-30 brightness-200 font-opensans text-3xl top-10 left-[30%] md:left-[30%] md:top-[27%] lg:top-[38%] lg:left-[25%]   xl:right-[25%]  xl:text-[3rem]  ">Admissions  <br className="md:hidden" /><span className="font-Roboto relative left-3 md:left-0 font-bold w-fit  text-[#A02021]">Open 2025</span></p>
       {/* <span className="font-opensans font-bold lg:text-3xl w-fit   mx-auto absolute xl:top-[35%] xl:left-[40%] text-red-500 z-30 brightness-200 text-2xl  top-20 left-[37%] md:left-[45%] md:top-[33%] hidden">Open 2025</span>               */}
-      <p className="font-bold text-3xl w-fit my-2  mx-auto absolute top-[48%]  left-[38%] text-white z-30 md:left-[45%] md:top-[32%] lg:top-[46%] xl:top-[44%] xl:left-[45%]"><ApplyNowButton/></p>
+      <p className="font-bold text-3xl w-fit my-2  mx-auto absolute top-[48%]  left-[38%] text-white z-30 md:left-[45%] md:top-[32%] lg:top-[46%] xl:top-[44%] xl:left-[46%] xl:text-[3rem]"><ApplyNowButton/></p>
            
       <div className=" opacity-90 ">
         <ImagesSlider className="h-60 md:h-[45rem]" images={images}>
